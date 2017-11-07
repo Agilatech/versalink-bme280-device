bf6b2332c90d35669f0596e2487235bff999d3a3
Agilatech <scott@agilatech.com>
2017-11-07 13:16:41 -0700

    Change scout to class syntax

--- bme280-scout.js
+++ bme280-scout.js
@@ -2,53 +2,52 @@
 const config = require('./config');
 
 const Scout = require('zetta-scout');
-const bme280 = require('./bme280');
-const util = require('util');
+const Bme280 = require('./bme280');
 
-const Bme280Scout = module.exports = function(opts) {
-    
-  // see if any of the config options were overridden in the server
+module.exports = class Bme280Scout extends Scout {
 
-  if (typeof opts !== 'undefined') {
-    // copy all config options defined in the server
-    for (const key in opts) {
-      if (typeof opts[key] !== 'undefined') {
-        config[key] = opts[key];
+  constructor(opts) {
+
+    super();
+
+    if (typeof opts !== 'undefined') {
+      // copy all config options defined in the server
+      for (const key in opts) {
+        if (typeof opts[key] !== 'undefined') {
+          config[key] = opts[key];
+        }
       }
     }
+
+    if (config.name === undefined) { config.name = "BME280" }
+    this.name = config.name;
+
+    this.bme280 = new Bme280(config);
+
   }
 
-  if (config.name == 'undefined') { config.name = "BME280" }
-  this.name = config.name;
-    
-  this.Bme280 = new bme280(config);
-
-  Scout.call(this);
-};
-
-util.inherits(Bme280Scout, Scout);
-
-Bme280Scout.prototype.init = function(next) {
-
-  const query = this.server.where({name: this.name});
+  init(next) {
+    const query = this.server.where({name: this.name});
   
-  const self = this;
-  this.server.find(query, function(err, results) {
-    if (!err) {
-      if (results[0]) {
-        self.provision(results[0], self.bme280);
-        self.server.info('Provisioned known device ' + self.name);
-      } else {
-        self.discover(self.bme280);
-        self.server.info('Discovered new device ' + self.name);
+    const self = this;
+
+    this.server.find(query, function(err, results) {
+      if (!err) {
+        if (results[0]) {
+          self.provision(results[0], self.bme280);
+          self.server.info('Provisioned known device ' + self.name);
+        } else {
+          self.discover(self.bme280);
+          self.server.info('Discovered new device ' + self.name);
+        }
       }
-    }
-    else {
-      self.server.error(err);
-    }
-  });
+      else {
+        self.server.error(err);
+      }
+    });
 
+    next();
+  }
 
-  next();
+}
 
-};
